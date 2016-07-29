9b2a1bf76d95d8d11eea8274608259cd87238868
jiangnenghua <568263091@qq.com>
2016-07-29 18:42:19 +0800

    回去创建日志类

---
+++ src/main/java/com/fibbery/framework/aop/ControllerLogAspect.java
@@ -0,0 +1,7 @@
+package com.fibbery.framework.aop;
+
+/**
+ * Created by Administrator on 2016/7/29.
+ */
+public class ControllerLogAspect {
+}
---
+++ src/main/java/com/fibbery/framework/aop/annotation/ControllerLog.java
@@ -0,0 +1,7 @@
+package com.fibbery.framework.aop.annotation;
+
+/**
+ * Created by Administrator on 2016/7/29.
+ */
+public @interface ControllerLog {
+}
