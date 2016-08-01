fd364b3a7443d81733f16f41d874b08f79ccb7ec
jiangnenghua <568263091@qq.com>
2016-08-01 15:57:14 +0800

    增加权限相关创建表语句:sys_resource,sys_user,sys_role,sys_user_role,sys_role_resource

--- src/script/2016-07-30/webbase_create.sql
+++ src/script/2016-07-30/webbase_create.sql
@@ -14,17 +14,57 @@ status smallint comment '状态'
 );
 
 #创建资源表
-create table if not exists sys_resource()
+create table if not exists sys_resource(
+  id bigint not null primary key auto_increment,
+  create_date DATE comment '创建时间',
+  create_user bigint  comment '创建人',
+  last_update_date date comment '最后更新时间',
+  last_update_user bigint comment '最后更新人',
+  status smallint comment '状态',
+  name varchar(25) not null comment '名称',
+  type int not null default 1 comment '资源类型',
+  href varchar(255),
+  parent bigint comment '父节点',
+  levels int not null default 0 comment '级次'
+)
 
 
 # 创建人员表
 create table if not exists sys_user(
-	
+  id bigint not null primary key auto_increment,
+  create_date DATE comment '创建时间',
+  create_user bigint  comment '创建人',
+  last_update_date date comment '最后更新时间',
+  last_update_user bigint comment '最后更新人',
+  status smallint comment '状态',
+  name varchar(20) not null comment '用户名',
+  account varchar(20) not null comment '账户',
+  password varchar(20) not null comment '密码(MD5加密存储)'
 )
 
 #创建角色表
 create table if not exists sys_role(
-
+  id bigint not null primary key auto_increment,
+  create_date DATE comment '创建时间',
+  create_user bigint  comment '创建人',
+  last_update_date date comment '最后更新时间',
+  last_update_user bigint comment '最后更新人',
+  status smallint comment '状态',
+  name varchar(20) not null comment '角色名',
+  isAdmin int not null default 0 comment '是否超级管理员'
 )
 
-#创建权限表
+#创建用户角色表
+create table if not exists sys_user_role(
+  user_id bigint not null comment '用户ID',
+  role_id bigint not null comment '角色ID',
+  primary key(user_id, role_id)
+)
+
+
+#创建角色资源表
+create table if not exists sys_role_resource(
+  role_id bigint not null comment '角色ID',
+  resource_id bigint not null comment '资源ID',
+  primary key(role_id, resource_id)
+)
