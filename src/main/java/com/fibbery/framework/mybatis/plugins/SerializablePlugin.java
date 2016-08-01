7b2c308b24fdf407510a2d7795b84651c8618fbc
jiangnenghua <568263091@qq.com>
2016-08-01 18:19:35 +0800

    Mybatis serialize插件开发

---
+++ src/main/java/com/fibbery/framework/mybatis/plugins/SerializablePlugin.java
@@ -0,0 +1,57 @@
+package com.fibbery.framework.mybatis.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.Plugin;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * mybatis生成类的序列化工具
+ * Created by Administrator on 2016/8/1.
+ */
+public class SerializablePlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return super.modelBaseRecordClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return super.modelExampleClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return super.modelGetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+
+    @Override
+    public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return super.modelPrimaryKeyClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    @Override
+    public boolean modelRecordWithBLOBsClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        return super.modelRecordWithBLOBsClassGenerated(topLevelClass, introspectedTable);
+    }
+
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return super.modelSetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+    }
+}
---
+++ src/main/java/com/fibbery/framework/vo/handler/ResourceTypeEnumHandler.java
@@ -0,0 +1,53 @@
+package com.fibbery.framework.vo.handler;
+
+import com.fibbery.framework.vo.enums.ResourceTypeEnum;
+import com.fibbery.framework.vo.enums.StatusEnum;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+public class ResourceTypeEnumHandler extends BaseTypeHandler<ResourceTypeEnum> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, ResourceTypeEnum parameter, JdbcType jdbcType) throws SQLException {
+        ps.setInt(i, parameter.getCode());
+    }
+
+    @Override
+    public ResourceTypeEnum getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        int code = rs.getInt(columnName);
+        if( rs.wasNull()){
+            return null;
+        }else {
+            return locateResourceTypeEnum(code);
+        }
+    }
+
+    private ResourceTypeEnum locateResourceTypeEnum(int code) {
+        ResourceTypeEnum[] enums = ResourceTypeEnum.values();
+        for(ResourceTypeEnum status: enums){
+            if(status.getCode() == code){
+                return status;
+            }
+        }
+
+        throw  new IllegalArgumentException("未知的code:"+code+",请检查");
+    }
+
+    @Override
+    public ResourceTypeEnum getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ResourceTypeEnum getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return null;
+    }
+}
