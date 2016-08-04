eba61443d25cace62ab6b90f488f12e6503fa852
jiangnenghua <568263091@qq.com>
2016-08-04 18:29:58 +0800

    1.去除所谓的基类,感觉并没有什么用 2.添加ComentPlugins

---
+++ src/main/java/com/fibbery/framework/mybatis/plugins/CommentPlugin.java
@@ -0,0 +1,118 @@
+package com.fibbery.framework.mybatis.plugins;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.PropertyRegistry;
+
+import java.util.List;
+import java.util.Properties;
+
+import static org.mybatis.generator.internal.util.StringUtility.isTrue;
+
+/**
+ * @see org.mybatis.generator.internal.DefaultCommentGenerator
+ * 依赖 persistence的注解包 用来注释生成的model
+ * Created by jiangnenghua on 2016/8/4.
+ */
+
+public class CommentPlugin implements CommentGenerator {
+
+    /** The properties. */
+    private Properties properties;
+
+    /** The suppress date. */
+    private boolean suppressDate;
+
+    /** The suppress all comments. */
+    private boolean suppressAllComments;
+
+    /** The addition of table remark's comments.
+     * If suppressAllComments is true, this option is ignored*/
+    private boolean addRemarkComments;
+
+
+    public CommentPlugin() {
+        super();
+        properties = new Properties();
+        suppressDate = false;
+        suppressAllComments = false;
+        addRemarkComments = false;
+    }
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        this.properties.putAll(properties);
+
+        suppressDate = isTrue(properties
+                .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_DATE));
+
+        suppressAllComments = isTrue(properties
+                .getProperty(PropertyRegistry.COMMENT_GENERATOR_SUPPRESS_ALL_COMMENTS));
+
+        addRemarkComments = isTrue(properties
+                .getProperty(PropertyRegistry.COMMENT_GENERATOR_ADD_REMARK_COMMENTS));
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+
+    }
+}
